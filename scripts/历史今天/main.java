347df07d771d462c2133c2b0a0d8af0626ff5592
Angus <89133686+angusdevgo@users.noreply.github.com>
2026-08-22 05:16:42 +0800

    Add script 历史今天 (历史今天)

---
+++ scripts/历史今天/main.java
@@ -0,0 +1,28 @@
+void sendToday(String talker) {
+    var api = "https://v2.xxapi.cn/api/historypic"
+    get(api, null, respContent -> {
+        var jsonObj = new JSONObject(respContent)
+        var code = jsonObj.optInt("code")
+        if (code == 200) {
+            var url = jsonObj.optString("data")
+            var path = "${cacheDir}/image.png"
+            download(url, path, null, cacheFile -> {
+                sendImage(talker, cacheFile.getAbsolutePath())
+                delay(3000,() -> {
+                    cacheFile.delete()
+                });
+              
+            })
+        }
+    })
+}
+
+void onHandleMsg(Object msgInfoBean) {
+    if (msgInfoBean.isText()) {
+        var content = msgInfoBean.getContent()
+        var talker = msgInfoBean.getTalker()
+        if (content.equals("/历史今天")) {
+            sendToday(talker)
+        }
+    }
+}
